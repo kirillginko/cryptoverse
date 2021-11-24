366c134bb4abc04336b78520fbb892e6972b0726
Kirill Ginko <kirillginko@gmail.com>
2021-11-23 20:51:43 -0500

    added crypto api info and redux store

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CryptoDetails() {
+  return <div></div>;
+}
+
+export default CryptoDetails;
---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Exchanges() {
+  return <div></div>;
+}
+
+export default Exchanges;
